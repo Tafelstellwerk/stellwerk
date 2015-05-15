8fbfade41bc4434145de627b3387c5da7ba8de80
Max Fellmuth <max@maxfellmuth.com>
2015-05-15 23:28:26 +0200

    feat(user): add role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  enum role: [:admin, :coordinator, :donor]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :donor
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
