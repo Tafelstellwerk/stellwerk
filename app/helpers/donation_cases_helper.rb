1e3e554e5b5b00e567418aa0a5c9b189eb67796e
Paul Platenius <Platte79@gmx.net>
2016-02-21 01:41:20 +0100

    remove donation_case helper, use underlying object in fields_for

--- app/helpers/donation_cases_helper.rb
+++ app/helpers/donation_cases_helper.rb
@@ -1,6 +1,2 @@
 module DonationCasesHelper
-  def attribute_changed(donation, attribute)
-    return false unless @donations_changeset.has_key?(donation)
-    @donations_changeset[donation].has_key?(attribute)
-  end
 end
